37e9a3f6772ecb0de07a519c19df34d18a0d2e7a
Pierre-Hugues Husson <pierre-hugues.husson@softathome.com>
2018-08-29 13:19:34 +0200

    [Misc] Move spListener back to object, to keep reference

--- app/src/main/java/me/phh/treble/app/Misc.kt
+++ app/src/main/java/me/phh/treble/app/Misc.kt
@@ -10,31 +10,35 @@ import vendor.huawei.hardware.biometrics.fingerprint.V2_1.IExtBiometricsFingerpr
 import vendor.huawei.hardware.tp.V1_0.ITouchscreen
 
 object Misc: EntryStartup {
-    override fun startup(ctxt: Context) {
-        val spListener = SharedPreferences.OnSharedPreferenceChangeListener { sp, key ->
-            when(key) {
-                MiscSettings.mobileSignal -> {
-                    val value = sp.getString(key, "default")
-                    SystemProperties.set("persist.sys.signal.level", value)
-                    Log.d("PHH", "Setting signal level method to $value")
-                }
-                MiscSettings.fpsDivisor -> {
-                    val value = sp.getString(key, "1")
-                    Log.d("PHH", "Setting fps divisor to $value")
-                    Settings.Global.putString(ctxt.applicationContext.contentResolver, "fps_divisor", value)
-                }
-                MiscSettings.maxAspectRatioPreO -> {
-                    val value = sp.getString(key, "1.86")
-                    SystemProperties.set("persist.sys.max_aspect_ratio.pre_o", value)
-                    Log.d("PHH", "Setting max aspect ratio for pre-o app $value")
-                }
+    lateinit var ctxt: Context
+    val spListener = SharedPreferences.OnSharedPreferenceChangeListener { sp, key ->
+        when(key) {
+            MiscSettings.mobileSignal -> {
+                val value = sp.getString(key, "default")
+                SystemProperties.set("persist.sys.signal.level", value)
+                Log.d("PHH", "Setting signal level method to $value")
+            }
+            MiscSettings.fpsDivisor -> {
+                val value = sp.getString(key, "1")
+                Log.d("PHH", "Setting fps divisor to $value")
+                Settings.Global.putString(ctxt.contentResolver, "fps_divisor", value)
+            }
+            MiscSettings.maxAspectRatioPreO -> {
+                val value = sp.getString(key, "1.86")
+                SystemProperties.set("persist.sys.max_aspect_ratio.pre_o", value)
+                Log.d("PHH", "Setting max aspect ratio for pre-o app $value")
             }
         }
+    }
+
+    override fun startup(ctxt: Context) {
 
         if (!MiscSettings.enabled()) return
 
         val sp = PreferenceManager.getDefaultSharedPreferences(ctxt)
         sp.registerOnSharedPreferenceChangeListener(spListener)
+
+        this.ctxt = ctxt.applicationContext
 
         //Refresh parameters on boot
         spListener.onSharedPreferenceChanged(sp, MiscSettings.fpsDivisor)
