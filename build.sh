ad9057eda3dc93cbb516324cfec701f3cf6cb25f
Pierre-Hugues Husson <pierre-hugues.husson@softathome.com>
2018-09-03 10:28:11 +0200

    Add "release" build.sh target

--- build.sh
+++ build.sh
@@ -6,5 +6,13 @@ if [ -z "$ANDROID_HOME" ];then
     export ANDROID_HOME=$PWD/sdk
 fi
 
-./gradlew assembleDebug
-LD_LIBRARY_PATH=./signapk/ java -jar signapk/signapk.jar keys/platform.x509.pem keys/platform.pk8 ./app/build/outputs/apk/debug/app-debug.apk app.apk
+gradleTarget=assembleDebug
+target=debug
+file=app-debug
+if [ "$1" == "release" ];then
+    gradleTarget=assembleRelease
+    target=release
+    file=app-release-unsigned
+fi
+./gradlew $gradleTarget
+LD_LIBRARY_PATH=./signapk/ java -jar signapk/signapk.jar keys/platform.x509.pem keys/platform.pk8 ./app/build/outputs/apk/$target/${file}.apk app.apk
