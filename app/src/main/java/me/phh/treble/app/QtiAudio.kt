3d403df589310b5a74d88d7fec856948b43a115c
Pierre-Hugues Husson <phh@phh.me>
2018-09-03 23:38:34 +0200

    Detect CAF IMS apk and enable RRO and send CHANGED intent to trigger it

--- app/src/main/java/me/phh/treble/app/QtiAudio.kt
+++ app/src/main/java/me/phh/treble/app/QtiAudio.kt
@@ -1,7 +1,12 @@
 package me.phh.treble.app
 
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.AudioSystem
+import android.net.Uri
+import android.os.SystemProperties
+import android.os.UserHandle
 import android.util.Log
 import dalvik.system.PathClassLoader
 import kotlin.concurrent.thread
@@ -42,6 +47,23 @@ class QtiAudio: EntryStartup {
                 } catch (e: Exception) {
                     Log.d("PHH", "Failed setting vendor.qti.hardware.radio.am $slot cb $e")
                 }
+            }
+
+            Log.d("PHH", "Checking CAF IMS status")
+            val installed = ctxt.packageManager.getInstalledPackages(0).find { it.packageName == "org.codeaurora.ims" } != null
+            val imsRroProperty = "persist.sys.phh.ims.caf"
+            Log.d("PHH", "CAF IMS $installed installed")
+            if(installed) {
+                SystemProperties.set(imsRroProperty, "true")
+                val replaceIntent =
+                        Intent(Intent.ACTION_PACKAGE_CHANGED)
+                                .setData(Uri.parse("package:org.codeaurora.ims"))
+                                .putExtra(Intent.EXTRA_UID, 0)
+                                .putExtra(Intent.EXTRA_DONT_KILL_APP, false)
+                                .putExtra(Intent.EXTRA_CHANGED_COMPONENT_NAME_LIST, emptyArray<String>())
+                ctxt.sendBroadcastAsUser(replaceIntent, UserHandle.SYSTEM)
+            } else {
+                SystemProperties.set(imsRroProperty, "false")
             }
         }
     }
