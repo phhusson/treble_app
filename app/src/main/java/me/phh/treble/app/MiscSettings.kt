0c632d24e2eb12dd467c1bb78dfef1f1c0c1cb7c
Pierre-Hugues Husson <phh@phh.me>
2018-09-05 21:15:13 +0200

    Delete dead code

--- app/src/main/java/me/phh/treble/app/MiscSettings.kt
+++ app/src/main/java/me/phh/treble/app/MiscSettings.kt
@@ -1,6 +1,5 @@
 package me.phh.treble.app
 
-import android.hidl.manager.V1_1.IServiceManager
 import android.os.Bundle
 import android.os.SystemProperties
 import android.preference.PreferenceFragment
