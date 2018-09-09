ffe8cc93720e2226265ec8880583f82899d12bc6
Pierre-Hugues Husson <phh@phh.me>
2018-09-09 23:07:09 +0200

    Add Samsung glove mode support

--- app/src/main/java/me/phh/treble/app/SamsungSettings.kt
+++ app/src/main/java/me/phh/treble/app/SamsungSettings.kt
@@ -6,6 +6,7 @@ import android.preference.PreferenceFragment
 
 object SamsungSettings {
     val highBrightess = "key_samsung_high_brightness"
+    val gloveMode = "key_samsung_glove_mode"
 
     fun enabled(): Boolean =
             Tools.vendorFpLow.matches(Regex(".*(crown|star)[q2]*lte.*"))
