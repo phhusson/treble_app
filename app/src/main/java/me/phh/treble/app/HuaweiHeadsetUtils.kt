875d4753739819ba736b4101ebbdfaf08b8d5b22
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-04-06 10:59:05 +0100

    Lock and restore mode after

--- app/src/main/java/me/phh/treble/app/HuaweiHeadsetUtils.kt
+++ app/src/main/java/me/phh/treble/app/HuaweiHeadsetUtils.kt
@@ -2,22 +2,33 @@ package me.phh.treble.app
 
 
 import android.media.AudioManager
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 object HuaweiHeadsetUtils {
+    private val lock = ReentrantLock()
+    private var mode: Int = -1
     fun speaker(audioManager: AudioManager) {
-        audioManager.mode = AudioManager.MODE_IN_COMMUNICATION
-        audioManager.isSpeakerphoneOn = true
-        // Again. Who knows why, but otherwise it just stays in in-call speakerphone
-        audioManager.mode = AudioManager.MODE_IN_COMMUNICATION
-        audioManager.mode = AudioManager.MODE_NORMAL
+        lock.withLock {
+            mode = audioManager.mode
+            audioManager.mode = AudioManager.MODE_IN_COMMUNICATION
+            audioManager.isSpeakerphoneOn = true
+            // Again. Who knows why, but otherwise it just stays in in-call speakerphone
+            audioManager.mode = AudioManager.MODE_IN_COMMUNICATION
+            audioManager.mode = AudioManager.MODE_NORMAL
+            audioManager.mode = mode
+        }
     }
 
     fun headset(audioManager: AudioManager) {
-        audioManager.isSpeakerphoneOn = false
-        audioManager.mode = AudioManager.MODE_IN_COMMUNICATION
-        audioManager.isSpeakerphoneOn = false
-        audioManager.mode = AudioManager.MODE_NORMAL
-        audioManager.isSpeakerphoneOn = false
+        lock.withLock {
+            mode = audioManager.mode
+            audioManager.isSpeakerphoneOn = false
+            audioManager.mode = AudioManager.MODE_IN_COMMUNICATION
+            audioManager.isSpeakerphoneOn = false
+            audioManager.mode = AudioManager.MODE_NORMAL
+            audioManager.isSpeakerphoneOn = false
+            audioManager.mode = mode
+        }
     }
-
 }
