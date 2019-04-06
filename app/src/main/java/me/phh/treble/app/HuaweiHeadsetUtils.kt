d89ad7db6ecaf1bafb10bdbd115b213f2f90667f
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-04-06 08:30:53 +0100

    Make it faster

--- app/src/main/java/me/phh/treble/app/HuaweiHeadsetUtils.kt
+++ app/src/main/java/me/phh/treble/app/HuaweiHeadsetUtils.kt
@@ -7,13 +7,9 @@ object HuaweiHeadsetUtils {
     fun speaker(audioManager: AudioManager) {
         audioManager.mode = AudioManager.MODE_IN_COMMUNICATION
         audioManager.isSpeakerphoneOn = true
-        audioManager.mode = AudioManager.MODE_NORMAL
-        audioManager.isSpeakerphoneOn = true
         // Again. Who knows why, but otherwise it just stays in in-call speakerphone
         audioManager.mode = AudioManager.MODE_IN_COMMUNICATION
-        audioManager.isSpeakerphoneOn = true
         audioManager.mode = AudioManager.MODE_NORMAL
-        audioManager.isSpeakerphoneOn = true
     }
 
     fun headset(audioManager: AudioManager) {
